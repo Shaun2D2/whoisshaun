897ffcf7a496814118fcb55beb2da7a4c96545ac
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2019-01-13 18:45:15 -0700

    removed some junk from the template, added footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,13 +6,20 @@ const Footer = () => (
 		<div className="container">
 			<div className="row">
 				<div className="col-md-4">
-					<span className="copyright">Copyright &copy; Shaun Lobsinger {moment().format("YYYY")}</span>
+					<span className="copyright">Shaun Lobsinger {moment().format("YYYY")}</span>
 				</div>
-				<div className="col-md-4">
+				<div className="col-md-4 offset-md-4">
 					<ul className="list-inline social-buttons">
-						<li><a href="https://www.linkedin.com/in/shaun-lobsinger-b4098066"><i className="fa fa-linkedin"></i></a></li>
-						<li><a href="https://twitter.com/Shaun2D2"><i className="fa fa-twitter"></i></a></li>
-						<li><a href="https://github.com/Shaun2D2"><i className="fa fa-github"></i></a></li>
+						<li className="list-inline-item">
+							<a href="https://github.com/Shaun2D2">
+								<i className="fab fa-github-alt" />
+							</a>
+						</li>
+						<li className="list-inline-item">
+							<a href="https://www.linkedin.com/in/shaun-lobsinger-b4098066">
+								<i className="fab fa-linkedin-in" />
+							</a>
+						</li>
 					</ul>
 				</div>
 			</div>
